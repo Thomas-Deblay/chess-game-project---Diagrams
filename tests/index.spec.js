72c1ac4ef0c981c7e6b0d8d257f0942c75f26195
Thomas-Deblay <thomasdelay.dev@gmail.com>
2024-01-29 00:31:16 +0100

    Initiation of all project files and folder with : html, css, js, jasmine and tests and img folder

---
+++ tests/index.spec.js
@@ -0,0 +1,8 @@
+describe('CHESS PROJECT', () => {
+  describe('class', () => {
+    it('should be defined', () => {
+      // Check if the Question class is defined
+      expect(Question).toBeDefined();
+    });
+  });
+});
